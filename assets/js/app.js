88fffddcaae1d43315236102291aa0a9452b6001
MarionFoto <marionacp@gmail.com>
2017-12-13 19:25:38 -0300

    footmap con estructura falta funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	/*$('.uno').hide(); // escondo columns uno
+	$('.uno').hide(); /*escondo columns uno*/
 	setTimeout(function() {
         $(".cero").fadeOut(1500);
-    },3000);*/
+    },3000);
 
     setTimeout(function() {
         $(".uno").fadeIn(1500);
@@ -15,33 +15,59 @@ $('.pictures').mouseover(function () {
     $('.comida-overlay').hide();
 });
 
-//selecion de comida
-var tipoEat = $('#select').val();
-var valor = $('#select option:selected').text()
+/*seleccion de comida
+var container = $('.box');
+$('#select').on('change', function(){
+    var eat = $('#select').val();
+    if(eat === "value1") {
+    box.html(<div class="column is-4 eat">
+                        <figure class="image is-96x96">
+                            <img src="assets/img/vegeta2.jpg" class="comida">
+                            <div class="comida-overlay">
+                                <p>Del huerto</p>
+                            </div>
+                        </figure>   
+                    </div>);
+    }
+    if(eat === "value2") {
+        container.html(<div class="column is-4">
+                        <figure class="image is-96x96">
+                            <img src="assets/img/buffet2.jpeg" class="comida">
+                            <div class="comida-overlay">
+                                <p> ¡Tenedor libre! </p>
+                            </div>
+                        </figure>
+                    </div>);
+    }
 
-//seleccion modal
-$("#select").change(function() {
- $("select").click(function(){
-	$(".modal1").show();
+     if(eat === "value3") {
+          container.html(<div class="column is-4">
+                        <figure class="image is-96x96">
+                            <img src="assets/img/parrilla2.jpg" class="comida">
+                            <div class="comida-overlay">
+                                <p>Buenos Amigos</p>
+                            </div>
+                        </figure>
+                    </div>);
+    }
+
+
+})*/
+
+//modal1
+$('#v').on('change', function () {
+    $('.modal1').show();
 });
 
+//modal2
+$('#b').on('change', function () {
+    $('.modal2').show();
+});
+//modal3
+$('#p').on('change', function () {
+    $('.modal3').show();
+});
 
-
-
-//seleccion ejemplo modal
-/* 
-$("select").click(function(){
-	$(".modal").show();
 })
-*/
-
-//cerrar modal
-$('.fa fa-times').clik(function){
-	$('#ex1').hide();
-}
-
-
-
-});
 
   
