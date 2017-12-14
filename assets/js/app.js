e721e2dbdf242a20bf8ae318c3c45e79b8e93d6f
MarionFoto <marionacp@gmail.com>
2017-12-14 09:57:36 -0300

    tratar de arreglarlos modales 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,63 +8,64 @@ $(document).ready(function() {
         $(".uno").fadeIn(1500);
     },3000);
 
-//efecto mouse
-$('.pictures').mouseover(function () {
+/*efecto mouse*/
+$('#pictures').mouseover(function () {
     $('.comida-overlay').show();
 }).mouseout(function () {
     $('.comida-overlay').hide();
 });
 
+
 /*seleccion de comida
-var container = $('.box');
+var container = $('#container'); 
 $('#select').on('change', function(){
     var eat = $('#select').val();
-    if(eat === "value1") {
-    box.html(<div class="column is-4 eat">
+    if(eat === "1") {
+    container.html('<div class="column is-4" id="v">
                         <figure class="image is-96x96">
-                            <img src="assets/img/vegeta2.jpg" class="comida">
+                            <img src="assets/img/vegeta2" class="comida">
                             <div class="comida-overlay">
                                 <p>Del huerto</p>
                             </div>
                         </figure>   
-                    </div>);
+                    </div>' );
     }
-    if(eat === "value2") {
-        container.html(<div class="column is-4">
+    if(eat === "2") {
+        container.html('<div class="column is-4" id="b">
                         <figure class="image is-96x96">
                             <img src="assets/img/buffet2.jpeg" class="comida">
                             <div class="comida-overlay">
                                 <p> ¡Tenedor libre! </p>
                             </div>
                         </figure>
-                    </div>);
+                    </div>');
     }
 
-     if(eat === "value3") {
-          container.html(<div class="column is-4">
+     if(eat === "3") {
+          container.html('<div class="column is-4" id="p">
                         <figure class="image is-96x96">
                             <img src="assets/img/parrilla2.jpg" class="comida">
                             <div class="comida-overlay">
                                 <p>Buenos Amigos</p>
                             </div>
                         </figure>
-                    </div>);
+                    </div>');
     }
 
 
 })*/
 
-//modal1
-$('#v').on('change', function () {
+/*modal1*/
+$('#v').click('change', function () {
     $('.modal1').show();
 });
 
-//modal2
-$('#b').on('change', function () {
+/*modal2*/
+$('#b').click('change', function () {
     $('.modal2').show();
 });
-//modal3
-$('#p').on('change', function () {
+/*modal3*/
+$('#p').click('change', function () {
     $('.modal3').show();
 });
 
